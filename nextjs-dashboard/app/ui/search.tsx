e7af5651c6f2fdc84b6a3bd8ad52dee22d457ea2
iosky07 <yoskitanjung07@gmail.com>
2024-12-21 08:11:43 +0700

    add search-3

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -9,8 +9,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const { replace } = useRouter();
 
     function handleSearch(term: string) {
-        console.log(`Searching... ${term}`);
-
         const params = new URLSearchParams(searchParams);
         if (term) {
             params.set('query', term);
